5c46ba5ca2ef13105b37909a28e49f4b38597766
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-09-11 20:16:48 +0530

    1.2: simplified developement

--- apps/react-demo/src/app/App.jsx
+++ apps/react-demo/src/app/App.jsx
@@ -2,25 +2,51 @@ import { useState } from 'react';
 import './../../../html-css-js-demo/styles.css';
 
 export function App() {
-  // 1. create a variable "count"
-  const [count, setCount] = useState(0);
+  const [steps, setSteps] = useState({
+    'Learn HTML': true,
+    'Learn CSS / SASS': true,
+    'JavaScript & DOM Manipulation': true,
+    'Learn Version Control': true,
+    'Learn TypeScript': true,
+    'React Basics with Hooks': true,
+    'CSS / Styling Frameworks': true,
+    'Redux Toolkit': true,
+    SEO: true,
+    Hosting: true,
+    'Using BaaS': true,
+    'Web Security': false,
+    'Unit & Integration testing': false,
+    'Building Mobile Apps': true,
+    'Building Desktop Apps': false,
+  });
 
-  // 2. update the variable "count"
-  function increaseCount() {
-    setCount(count + 1);
+  function updateSteps(e) {
+    setSteps({ ...steps, [e.target.name]: e.target.checked });
   }
 
   return (
     <main>
-      <h1>Counter app using React</h1>
-
-      <span>1-step Counter</span>
-      <pre>{count}</pre>
-
-      <span>5-step Counter</span>
-      <pre>{count*5}</pre>
-
-      <button onClick={increaseCount}>Click to increase the count</button>
+      <h1>
+        Beginner Steps to become a <span>Pro Frontend Engineer</span>
+      </h1>
+      <div>
+        <ul>
+          {Object.entries(steps).map(([step, islearned], i) => {
+            return (
+              <li key={i}>
+                <span>{i + 1}</span>
+                <input
+                  type="checkbox"
+                  name={step}
+                  checked={islearned}
+                  onChange={updateSteps}
+                />
+                <label htmlFor={step}>{step}</label>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </main>
   );
 }
